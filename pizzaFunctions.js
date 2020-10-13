b94caa18ffc2416c31e782148cf208dfde09456c
rea-red <72822499+rea-red@users.noreply.github.com>
2020-10-13 17:13:21 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,10 @@
+/*
+* A set of functions to change the background color
+*/
+// Change background to red
+function setRed() {
+    document.body.style.background = "Red";
+}
+function setWheat() {
+    document.body.style.background = "Wheat";
+}
